a54f5368df64ba408539098dd4440bd8e8fd62eb
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2024-03-16 19:01:55 +0200

    clang-format, action fix

--- sample/main.c
+++ sample/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <debug.h>
 
-int gdb_stub_main( int argc, char *argv[] );
+int gdb_stub_main(int argc, char *argv[]);
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-  volatile int i = 0;
+    volatile int i = 0;
 
-  init_scr();
-  gdb_stub_main(0, NULL);
-  scr_printf("i = %d\n", i += 2);
-  scr_printf("i = %d\n", i += 10);
+    init_scr();
+    gdb_stub_main(0, NULL);
+    scr_printf("i = %d\n", i += 2);
+    scr_printf("i = %d\n", i += 10);
 
-  asm("l: b l;");
+    asm("l: b l;");
 
-  return 0;
+    return 0;
 }
